b31db32552bca98048dfb82ccf77cb6bf9184d3d
Hugo Fernandez <128651362+huugoox@users.noreply.github.com>
2026-05-24 15:23:49 +0200

    More data

--- NordPoool/data/Load from API/load_capacities_apy.py
+++ NordPoool/data/Load from API/load_capacities_apy.py
@@ -37,8 +37,8 @@ AREAS = [
 
 MARKET = "DayAhead"
 
-START_DATE = date(2020, 1, 1)
-END_DATE = date(2020, 12, 31)
+START_DATE = date(2015, 1, 1)
+END_DATE = date(2019, 12, 31)
 
 # =========================
 # GET ACCESS TOKEN
